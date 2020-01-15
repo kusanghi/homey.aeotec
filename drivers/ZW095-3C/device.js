fb2a7ef227bf14aa648ab1138b5eb8a33c6b5b3b
kentaclub <42129672+kentaclub@users.noreply.github.com>
2020-01-15 10:11:22 +0700

    Get Vaule when start app

--- drivers/ZW095-3C/device.js
+++ drivers/ZW095-3C/device.js
@@ -5,27 +5,36 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 class ZW095 extends ZwaveDevice {
 
 	onMeshInit() {
+		// enable debugging
+		this.enableDebug();
 
+		// print the node's info to the console
+        this.printNode();
+        
         this.registerCapability('measure_power', 'METER', {
             getOpts: {
+                getOnStart: true,
 				pollInterval: 30000,
 			},
         });
 
         this.registerCapability('meter_power', 'METER', {
             getOpts: {
+                getOnStart: true,
 				pollInterval: 30000,
 			},
         });
 
         this.registerCapability('measure_voltage', 'METER', {
             getOpts: {
+                getOnStart: true,
 				pollInterval: 30000,
 			},
         });
 
         this.registerCapability('measure_current', 'METER', {
             getOpts: {
+                getOnStart: true,
 				pollInterval: 30000,
 			},
         });
